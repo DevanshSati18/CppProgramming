fa0375283ae6abc466a0b10090ff20a588679c65
Devansh Sati <devanshjsati@gmail.com>
2024-11-08 09:43:12 +0530

    Create variable.cpp

---
+++ 1)C++(basic)/3)Variable/variable.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 30;          // Integer variable
+    float salary = 56000.50; // Floating-point variable
+    double pi = 3.1415926535; // Double precision variable
+    char grade = 'B';      // Character variable
+
+    cout << "Age: " << age << endl;
+    cout << "Salary: " << salary << endl;
+    cout << "Value of Pi: " << pi << endl;
+    cout << "Grade: " << grade << endl;
+
+    return 0;
+}
